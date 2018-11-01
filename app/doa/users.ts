4ef0e79031f0f6b80fa6f063ad1ba9ff72af45af
Lorne Currie <lorne.currie@tepapa.govt.nz>
2018-11-01 15:16:13 +1300

    Added check to make sure we only create a unique user.

--- app/doa/users.ts
+++ app/doa/users.ts
@@ -7,15 +7,28 @@ import { IUserInstance } from "./../db/models/user";
 import {hashPassword} from "../helpers/users";
 
 function create(user: IUserInstance): Promise<any> {
-    return db.User.create({
-        email: user.email,
-        firstName: user.firstName,
-        id: uuid.v4(),
-        lastName: user.lastName,
-        password: hashPassword(user.password),
+    return db.User.findOne({ where: {email: user.email} }).then((data) => {
+        if (data) {
+            return Promise.reject(data);
+        }
+        return db.User.create({
+            email: user.email,
+            firstName: user.firstName,
+            id: uuid.v4(),
+            lastName: user.lastName,
+            password: hashPassword(user.password),
+        });
+    });
+}
+
+function findAll(): Promise<any> {
+    return db.User.findAll({
+        attributes: { exclude: ["password"]},
+        include: [{all: true}],
     });
 }
 
 export {
     create,
+    findAll,
 };
